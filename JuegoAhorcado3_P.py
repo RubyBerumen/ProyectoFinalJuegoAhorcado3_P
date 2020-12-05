248fcaa7a620c577308018fc2b21cbe4adea1d4f
Ruby Berumen <clararubipb15@gmail.com>
2020-12-04 18:43:45 -0600

    Se modificó el método verificar dela clase archivo

--- JuegoAhorcado3_P.py
+++ JuegoAhorcado3_P.py
@@ -9,21 +9,23 @@ import os
 class Archivo :
     
     def verificar(self):
-        
-        palabras=""
-        archivo=open("palabras.txt","r")
-        palabras=archivo.read()
-        archivo.close()
-        comas=0
-        i=0
-        
-        for i in range(i,len(palabras)):
-            if(palabras[i]==","):
-                comas+=1
-        if(comas>0):
-            print(f"El archivo contiene {comas} palabras")
-        elif(len(palabras)==0):
+        if not os.path.isfile("palabras.txt"): 
+            print("EL archivo no existe")
+            archivo=open("palabras.txt","w+")
+            archivo.close()
+            if os.path.isfile("palabras.txt"):
+                print("Se ha creado el archivo")
+        if not os.path.getsize("palabras.txt") > 0:
             print("El archivo esta vacio")
+            return True
+        else:
+            with open("palabras.txt", "r+") as f:
+                lineas = 0
+                for l in f:
+                    if l != "\n":
+                        lineas+=1
+                print(f"El archivo contiene {lineas} palabras")
+            return False
     
     def eliminar(self):
         if os.path.isfile("palabras.txt"):
